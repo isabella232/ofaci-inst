b310f0c4eddbc1faca172bd6a52859e580dc4228
fumihiko kakuma <kakuma@valinux.co.jp>
2015-01-07 13:33:38 +0900

    add jenkins slave tools. these are called from pre_test_hook and post_test_hook in devstack-gate.

---
+++ slave/81-tempest.sh
@@ -0,0 +1,33 @@
+# tempest.sh - DevStack extras script
+
+if is_service_enabled tempest; then
+    if [[ "$1" == "source" ]]; then
+        # no-op
+        :
+    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        # no-op
+        :
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        # no-op
+        :
+    elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
+        echo_summary "Add Tempest configuration"
+        iniset $TEMPEST_CONFIG compute ping_timeout 300
+        iniset $TEMPEST_CONFIG compute ssh_timeout 196
+        sudo ovs-vsctl set Controller br-int inactivity_probe=30000
+    elif [[ "$1" == "stack" && "$2" == "post-extra" ]]; then
+        # no-op
+        :
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        # no-op
+        :
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        # no-op
+        :
+    fi
+fi
+
---
+++ slave/get_ofalog.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#BASE=/opt/stack
+OFALOGDIR=ofagent
+OFALOGPATH=/tmp/${OFALOGDIR}
+OFALOGFILE=${OFALOGPATH}/ofagent.log
+
+function cp_if_exist {
+    local logname=$1
+    local dest=$2
+    if [ -f "$logname" ]; then
+        sudo cp $logname $dest/
+    fi
+}
+
+mkdir -p $OFALOGPATH
+cp_if_exist /var/log/openvswitch/ovs-ctl.log $OFALOGPATH
+cp_if_exist /var/log/openvswitch/ovs-ctl.log.1 $OFALOGPATH
+cp_if_exist /var/log/openvswitch/ovsdb-server.log $OFALOGPATH
+cp_if_exist /var/log/openvswitch/ovsdb-server.log.1 $OFALOGPATH
+cp_if_exist /var/log/openvswitch/ovs-vswitchd.log $OFALOGPATH
+cp_if_exist /var/log/openvswitch/ovs-vswitchd.log.1 $OFALOGPATH
+
+echo -e "\n>> ovs-appctl bridge/dump-flows br-int\n" >> $OFALOGFILE
+sudo ovs-appctl bridge/dump-flows br-int >> $OFALOGFILE
+echo -e "\n>> ovs-appctl dpif/show\n" >> $OFALOGFILE
+sudo ovs-appctl dpif/show >> $OFALOGFILE
+echo -e "\n>> ovs-ofctl -O openflow13 dump-ports-desc br-int\n" >> $OFALOGFILE
+sudo ovs-ofctl -O openflow13 dump-ports-desc br-int >> $OFALOGFILE
+echo -e "\n>> ovs-ofctl -O openflow13 dump-ports br-int\n" >> $OFALOGFILE
+sudo ovs-ofctl -O openflow13 dump-ports br-int >> $OFALOGFILE
+echo -e "\n>> ovs-ofctl -O openflow13 dump-flows br-int\n" >> $OFALOGFILE
+sudo ovs-ofctl -O openflow13 dump-flows br-int >> $OFALOGFILE
+
+sudo chown jenkins:jenkins -R $OFALOGPATH
+pushd /tmp
+tar czf ofagent_log.tar.gz $OFALOGDIR
+sudo mv ofagent_log.tar.gz ${BASE}/logs/
+rm -rf $OFALOGDIR
+popd
