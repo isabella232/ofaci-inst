eb0fa9cc1dd3fb84221d4c58b874290db591fd78
fumihiko kakuma <kakuma@valinux.co.jp>
2015-05-22 11:55:28 +0900

    Use master branch of the following repositories as default - devstack-gate - project-config - system-config

--- install_node.sh
+++ install_node.sh
@@ -11,11 +11,11 @@ function usage {
 export DEVSTACK_GATE_3PPRJ_BASE=osrg
 export DEVSTACK_GATE_3PBRANCH=ofaci
 export OSEXT_REPO="-b $DEVSTACK_GATE_3PBRANCH https://github.com/${DEVSTACK_GATE_3PPRJ_BASE}/os-ext-testing.git"
-export CONFIG_REPO="-b $DEVSTACK_GATE_3PBRANCH https://github.com/${DEVSTACK_GATE_3PPRJ_BASE}/system-config.git"
-export PROJECT_CONF_REPO=https://github.com/${DEVSTACK_GATE_3PPRJ_BASE}/project-config.git
-export DEVSTACK_GATE_REPO="-b $DEVSTACK_GATE_3PBRANCH https://github.com/${DEVSTACK_GATE_3PPRJ_BASE}/devstack-gate.git"
+#export CONFIG_REPO="-b $DEVSTACK_GATE_3PBRANCH https://github.com/${DEVSTACK_GATE_3PPRJ_BASE}/system-config.git"
+#export PROJECT_CONF_REPO=https://github.com/${DEVSTACK_GATE_3PPRJ_BASE}/project-config.git
+#export DEVSTACK_GATE_REPO="-b $DEVSTACK_GATE_3PBRANCH https://github.com/${DEVSTACK_GATE_3PPRJ_BASE}/devstack-gate.git"
 export INST_PUPPET_SH="https://raw.github.com/${DEVSTACK_GATE_3PPRJ_BASE}/system-config/master/install_puppet.sh"
-export ZUUL_REVISION=1f4f8e136ec33b8babf58c0f43a83860fa329e52
+#export ZUUL_REVISION=1f4f8e136ec33b8babf58c0f43a83860fa329e52
 
 if [ -z "$1" ]; then
     echo "node-type does not specify !"
